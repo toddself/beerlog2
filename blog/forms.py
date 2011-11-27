345d42a4338c02a3010683a64f9e17f5c042539a
todd <todd.kennedy@gmail.com>
2011-11-26 19:30:04 -0500

    switching to flask-wtf

--- blog/forms.py
+++ blog/forms.py
@@ -3,8 +3,8 @@ from __future__ import absolute_import
 
 from sqlobject import SQLObjectNotFound
 
-from wtforms import Form, TextField, TextAreaField, BooleanField, HiddenField, \
-                    validators, ValidationError
+from flaskext.wtf import Form, TextField, TextAreaField, BooleanField, \
+                         HiddenField, validators, ValidationError
 from wtforms.ext.dateutil.fields import DateTimeField
 
 def is_int(form, field):
@@ -14,8 +14,14 @@ def is_int(form, field):
         raise ValidationError('Must be a valid integer')
 
 class EntryForm(Form):
-    title = TextField('Title', [validators.Length(min=1, max=255, message="You must provide a title")])
-    body = TextAreaField('Body', [validators.Length(min=4, max=1048576, message="The body is required")])
+    title = TextField('Title', 
+                      [validators.Length(min=1, 
+                                         max=255, 
+                                         message="You must provide a title")])
+    body = TextAreaField('Body', 
+                         [validators.Length(min=4, 
+                                            max=1048576, 
+                                            message="The body is required")])
     time = DateTimeField('Time', display_format="%H:%M")
     date = DateTimeField('Date', display_format="%m/%d/%Y")
     tags = TextField('Tags')
