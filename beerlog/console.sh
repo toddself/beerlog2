06035e9066f2ac388bb6fb07441d75d1f72501cb
todd <todd.kennedy@gmail.com>
2011-11-29 07:25:49 -0500

    changing directory structure to allow for setup

--- beerlog/console.sh
+++ beerlog/console.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+source ../env/bin/activate
 PYTHONSTARTUP=.pyprofile.py /usr/bin/env python
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-CFG=$1
-
-if [ -z $CFG ]
-then
-    CFG="settings.cfg"
-fi
-
-BEERLOG_SETTINGS=$CFG /usr/bin/env python beerlog_tests.py
