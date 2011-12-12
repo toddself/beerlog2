96cca3367156ea327425911f224ca36fd6ec7bd1
Todd Kennedy <todd.kennedy@gmail.com>
2011-12-12 15:07:40 -0500

    upping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='beerlog',
-    version='0.56',
+    version='0.57',
     long_description=__doc__,
     packages=['beerlog',
              'beerlog.blog',
